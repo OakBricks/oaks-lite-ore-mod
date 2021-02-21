c72db9c8f26f2df3575a3697ee1d8023c8afb157
OakBricks <oakbricks@gmail.com>
2021-02-21 15:38:47 -0600

    Update ExampleMod.java

--- src/main/java/org/oakbricks/simpleores/ExampleMod.java
+++ src/main/java/org/oakbricks/simpleores/ExampleMod.java
@@ -2,7 +2,9 @@ package org.oakbricks.simpleores;
 
 import net.fabricmc.api.ModInitializer;
 
-public class ExampleMod implements ModInitializer {
+public class SimpleOres implements ModInitializer {
+    public static final Block EXAMPLE_BLOCK = new Block(FabricBlockSettings.of(Material.METAL).strength(4.0f));
+	
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
