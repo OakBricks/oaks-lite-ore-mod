ed9d5a062702fcd0dd004d96fdc93c3c6a8dd3e4
OakBricks <oakbricks@gmail.com>
2021-02-21 16:58:25 -0600

    idk lol

--- src/main/java/org/oakbricks/simpleores/SimpleOres.java
+++ src/main/java/org/oakbricks/simpleores/SimpleOres.java
@@ -1,16 +1,31 @@
 package org.oakbricks.simpleores;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import com.google.common.collect.ImmutableSet;
+import com.mojang.datafixers.util.Function5;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.player.UseBlockCallback;
+import net.fabricmc.fabric.api.tool.attribute.v1.FabricToolTags;
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.Util;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.world.World;
+import net.minecraft.world.poi.PointOfInterestType;
 
-public class SimpleOres implements ModInitializer {
+public class ExampleMod implements ModInitializer {
+	 
     public static final Block EXAMPLE_BLOCK = new Block(FabricBlockSettings.of(Material.METAL).strength(4.0f));
-	
-	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		System.out.println("The ores have appeared!");
-	}
+ 
+    @Override
+    public void onInitialize() {
+        Registry.register(Registry.BLOCK, new Identifier("tutorial", "example_block"), EXAMPLE_BLOCK);
+        Registry.register(Registry.ITEM, new Identifier("tutorial", "example_block"), new BlockItem(EXAMPLE_BLOCK, new FabricItemSettings().group(ItemGroup.MISC)));
+    }
 }
